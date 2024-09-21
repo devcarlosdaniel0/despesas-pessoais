f448ff47bf00da77c09ba1700d6d6ba1474768a8
Carlos <cadanielmarchesin@gmail.com>
2024-09-20 22:47:00 -0300

    Initial commit

---
+++ src/main/java/com/carlos/despesas_pessoais/DespesasPessoaisApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.despesas_pessoais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespesasPessoaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespesasPessoaisApplication.class, args);
+	}
+
+}
